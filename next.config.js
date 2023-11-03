4de8d45b64cf9fd40abef624136d097c2e019704
Renan Alves Medina <alvesrenansantos@hotmail.com>
2023-11-02 23:15:24 -0300

    test ci/cd

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
-import withImages from 'next-images'
+module.exports = {
+  output: "standalone",
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'dnafamilia.blob.core.windows.net',
-      'luminifirekeeper01.blob.core.windows.net',
-      'i.ytimg.com',
-    ],
-  },
+    "navigationFallback": {
+        "rewrite": "index.html",
+        "exclude": ["/static/images/*.{png,jpg,gif}", "/static/css/*"]
+    },
+    "mimeTypes": {
+        ".json": "text/json",
+        ".css": "text/css",
+        ".html": "text/html"
+    }
 }
+// import withImages from 'next-images'
 
-export default withImages(nextConfig)
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   reactStrictMode: true,
+//   images: {
+//     domains: [
+//       'dnafamilia.blob.core.windows.net',
+//       'luminifirekeeper01.blob.core.windows.net',
+//       'i.ytimg.com',
+//     ],
+//   },
+// }
+
+// export default withImages(nextConfig)
